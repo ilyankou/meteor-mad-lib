3383cc8c665ff15bb82b8f5d98a79ecfcfde6958
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2016-04-04 20:26:57 -0400

    Fade in the title

--- client/madLib.js
+++ client/madLib.js
@@ -2,15 +2,22 @@ Template.body.events({
   'submit form': function(event) {
       event.preventDefault();
 
-      const exclamation = document.getElementById('exclamation').value.trim();
-      const adverb = document.getElementById('adverb').value.trim();
-      const animal = document.getElementById('animal').value.trim();
-      const verb = document.getElementById('verb').value.trim();
+      const exclamation = $("#exclamation").val().trim();
+      const adverb = $("#adverb").val().trim();
+      const animal = $("#animal").val().trim();
+      const verb = $("#verb").val().trim();
 
       const madLib = "Oh, " + exclamation + "! My brother is a " + animal + " and he's " +
       adverb + " " + verb + "ing.";
 
 
       console.log(madLib);
+
+      $("#myTitle").text(madLib).css({"color": "#0000ff"}).fadeOut(0).fadeIn(2000);
   },
 });
+
+
+$(document).ready(function() {
+
+})
